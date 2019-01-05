b10f79f47598eeb1b5873cefa9b5116c571e0d41
Adsun <1216319784@qq.com>
2019-01-05 15:06:37 +0800

    commit

--- src/main/java/com/admin/repository/EditRepository.java
+++ src/main/java/com/admin/repository/EditRepository.java
@@ -11,12 +11,12 @@ import com.admin.entity.Edit;
 
 @Repository
 public interface EditRepository extends BaseRepository<Edit, Long> {
-	@Query(value="select u.id,u.title,u.sub_title,u.time,u.detail,u.img_url from ssm_edit u "
-			+ "where u.constant_id = ?1% order by u.update_datetime desc",
+	@Query(value="select u.id, u.title, u.sub_title, u.time, u.detail, u.img_url from ssm_edit u "
+			+ "where u.constant_id = ?1 order by u.update_datetime desc",
 			nativeQuery=true)
 	public List<Object[]> findConstantId(String constantId);
-	@Query(value="select u.id,u.title,u.sub_title,u.time,u.detail,u.img_url from ssm_edit u "
-			+ "where u.constant_id = ?1% order by u.update_datetime desc limit 5",
+	@Query(value="select u.id, u.title, u.sub_title, u.time, u.detail, u.img_url from ssm_edit u "
+			+ "where u.constant_id = ?1 order by u.update_datetime desc limit 5",
 			nativeQuery=true)
 	public List<Object[]> findConstantIdLimit(String constantId);
 	public Page<Edit> findByConstantIdInOrderByUpdateDatetimeDesc(List<String> constantIds, Pageable pageable);
